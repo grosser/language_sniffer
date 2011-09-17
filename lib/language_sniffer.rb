fc89e944cefe50736d8be198082588f049f6cdcc
grosser <grosser.michael@gmail.com>
2011-09-17 22:05:32 +0200

    test and fix top-level interface

--- lib/language_sniffer.rb
+++ lib/language_sniffer.rb
@@ -4,8 +4,7 @@ require 'language_sniffer/language'
 require 'language_sniffer/pathname'
 
 module LanguageSniffer
-  def self.detect(options)
-    raise "I need :path" if not options[:path]
-    FileBlob.new(options[:path], options[:pwd], options[:content])
+  def self.detect(path, options={})
+    FileBlob.new(path, options[:pwd], options[:content])
   end
 end
